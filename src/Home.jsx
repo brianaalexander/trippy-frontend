30ef176e1341ccf0612690812ab8f3a3c0d3dda9
brianaalexander <bribri0283@gmail.com>
2022-11-06 14:06:42 -0600

    added logoutlink

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,11 +1,13 @@
 import { Signup } from "./Signup";
 import { Login } from "./Login";
+import { LogoutLink } from "./LogoutLink";
 
 export function Home() {
   return (
     <div>
       <Signup />
       <Login />
+      <LogoutLink />
     </div>
   );
 }
