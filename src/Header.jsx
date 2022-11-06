d8586229a77a969f8826bbaab0b6f17efcfa1582
brianaalexander <bribri0283@gmail.com>
2022-11-06 16:22:55 -0600

    added nav-bar

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,8 +1,63 @@
+import { Link } from "react-router-dom";
+import { LogoutLink } from "./LogoutLink";
+
 export function Header() {
   return (
     <header>
-      <nav>
-        <a href="#">Home</a> | <a href="#">Link</a>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container-fluid">
+          <Link className="navbar-brand" to="/">
+            Trippy
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <Link className="nav-link active" aria-current="page" to="/">
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/trips">
+                  All Trips
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/trips/new">
+                  New Trip
+                </Link>
+              </li>
+              {localStorage.jwt === undefined ? (
+                <>
+                  <li className="nav-item">
+                    <Link className="nav-link" to="/signup">
+                      Signup
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link className="nav-link" to="/login">
+                      Login
+                    </Link>
+                  </li>
+                </>
+              ) : (
+                <li className="nav-item">
+                  <LogoutLink className="nav-link" />
+                </li>
+              )}
+            </ul>
+          </div>
+        </div>
       </nav>
     </header>
   );
