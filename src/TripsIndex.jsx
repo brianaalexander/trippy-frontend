a17412eced8e66545a8ca2c9c05ce8a365313791
brianaalexander <bribri0283@gmail.com>
2022-11-06 16:00:39 -0600

    made trips index cards in bootstrap

--- src/TripsIndex.jsx
+++ src/TripsIndex.jsx
@@ -1,15 +1,41 @@
+import { useState } from "react";
+
 export function TripsIndex(props) {
+  const [searchFilter, setSearchFilter] = useState("");
+
   return (
     <div>
-      <h1>All Trips</h1>
-      {props.trips.map((trip) => (
-        <div key={trip.id}>
-          <h2>{trip.title}</h2>
-          <img src={trip.image_url} />
-          <p>Arrival: {trip.start_time}</p>
-          <p>Departure: {trip.end_time}</p>
+      <div className="container" id="trips-index">
+        <h1>All Trips</h1>
+        Search filter:{" "}
+        <input
+          value={searchFilter}
+          onChange={(event) => setSearchFilter(event.target.value)}
+          type="text"
+          list="titles"
+        />
+        <datalist id="titles">
+          {props.trips.map((trip) => (
+            <option key={trip.id}>{trip.title}</option>
+          ))}
+        </datalist>
+        <div className="row">
+          {props.trips
+            .filter((trip) => trip.title.toLowerCase().includes(searchFilter.toLowerCase()))
+            .map((trip) => (
+              <div className="col-md-3 mb-4" key={trip.id}>
+                <div className="card">
+                  <img src={trip.image_url} className="card-img-top" alt="..." />
+                  <div className="card-body">
+                    <h2 className="card-title">{trip.title}</h2>
+                    <p className="card-text">Arrival: {trip.start_time}</p>
+                    <p className="card-text">Departure: {trip.end_time}</p>
+                  </div>
+                </div>
+              </div>
+            ))}
         </div>
-      ))}
+      </div>
     </div>
   );
 }
