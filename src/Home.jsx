14b6e139cb46dd9ec558f69bcbf5394f03414bf5
brianaalexander <bribri0283@gmail.com>
2022-11-07 20:36:11 -0600

    added picture to home page

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,8 +1,15 @@
 export function Home() {
   return (
     <div>
-      <h1>Welcome</h1>
-      <a href="/trips">all trips</a>
+      <h1>Time To See The world!</h1>
+      <img
+        className="welcome"
+        src="https://worldinparis.com/wp-content/uploads/2018/06/paris-travel-planning.jpg"
+        alt="..."
+      />
+      <a className="btn btn-outline-info" href="/trips">
+        All Trips
+      </a>
     </div>
   );
 }
