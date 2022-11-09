fb6791025d0afc1a7d3e3d25b226995887bb6ac5
brianaalexander <bribri0283@gmail.com>
2022-11-08 21:05:51 -0600

    added calendar on home page and centered under picture

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,4 +1,10 @@
+import React, { useState } from "react";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
+
 export function Home() {
+  const [value, onChange] = useState(new Date());
+
   return (
     <div id="home">
       <h1>Time To See The world!</h1>
@@ -7,6 +13,9 @@ export function Home() {
         src="https://worldinparis.com/wp-content/uploads/2018/06/paris-travel-planning.jpg"
         alt="..."
       />
+      <div className="mx-auto">
+        <Calendar onChange={onChange} value={value} />
+      </div>
       <a className="btn btn-outline-info" href="/trips">
         All Trips
       </a>
